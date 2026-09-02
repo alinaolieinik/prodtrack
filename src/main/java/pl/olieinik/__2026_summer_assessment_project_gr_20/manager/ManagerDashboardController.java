586b96d3cbdc4485679bfa3cb73723f23c575b0e
Alina Olieinik <alinaolieinik@student.kul.pl>
2026-09-02 19:39:48 +0200

    Adding manager dashboard + adding products and new functionalities

--- src/main/java/pl/olieinik/__2026_summer_assessment_project_gr_20/manager/ManagerDashboardController.java
+++ src/main/java/pl/olieinik/__2026_summer_assessment_project_gr_20/manager/ManagerDashboardController.java
@@ -1,12 +1,11 @@
-package pl.olieinik.__2026_summer_assessment_project_gr_20.dashboard;
+package pl.olieinik.__2026_summer_assessment_project_gr_20.manager;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.olieinik.__2026_summer_assessment_project_gr_20.manager.ManagerDashboardDto;
-import pl.olieinik.__2026_summer_assessment_project_gr_20.manager.ManagerDashboardService;
+
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
